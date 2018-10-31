749abe4a9fbe4abf3f5d7e4a20c671f1e44ed36f
dan_fred <dannyfrederick360@gmail.com>
2018-10-31 11:29:41 -0400

    finished computer decision

--- blackjack.py
+++ blackjack.py
@@ -5,6 +5,7 @@ Date: October 30, 2018
 Still need to add:
 - remove user's hidden card from used array visible to Computer
 - add support for ace to be 1 OR 11
+- finish computer's turn
 '''
 
 #remove my hidden card from computer's used array
@@ -22,30 +23,65 @@ class Game:
         self.cvalue = 0
         self.chand = [self.newCard(), self.newCard()]
 
-        self.run()
+        r = self.run()
+        if r == 7:
+            if self.value > self.cvalue:
+                print('You were colser to 21. You win!')
+                x = input('Press any key to exit --> ')
+                exit()
+            elif self.value < self.cvalue:
+                print('The computer was closer to 21. You lose!')
+                x = input('Press any key to exit --> ')
+                exit()
+        else:
+            x = input('Press any key to exit --> ')
+            exit()
 
+    #returns 1 if you get blackjack, 2 if you lose
+    # 5 if computer gets blackjack, and 6 if computer busts
+    # 7 if you both stay
     def run(self):
         print('Your visible card is ' + str(self.hand[0]))
         print('your opponents visible card is ' + str(self.chand[0]))
-
+        stay = False
+        cstay = False
         while True:
             #your turn
-            print('\nYour hand is valued at ' + str(self.value) + '.')
-            hit = input('Hit or stay --> ')
-            if hit == 'hit':
-                self.hand.append(self.newCard('user'))
-                if self.value == 21:
-                    print('You hit Blackjack!')
-                    break
-                if self.value > 21:
-                    print('You busted')
-                    break
+            if stay == False:
+                print('\nYour hand is valued at ' + str(self.value) + '.')
+                hit = input('Hit or stay --> ')
+                if hit == 'hit':
+                    self.hand.append(self.newCard('user'))
+                    if self.value == 21:
+                        print('You hit Blackjack!')
+                        return 1
+                    if self.value > 21:
+                        print('You busted. Computer wins!')
+                        return 2
 
-            if hit == 'stay':
-                print('Your final score is ' + str(self.value) + '.')
+                if hit == 'stay':
+                    print('Your final score is ' + str(self.value) + '.')
+                    stay = True
 
             #computer's turn
-            print("Computer's turn... ")
+                if cstay == False:
+                    print("Computer's turn... ")
+                    choice = self.choose()
+                if choice == 'stay':
+                    print('Computer chose to stay')
+                    cstay = True
+                if choice == 'hit':
+                    self.chand.append(self.newCard('computer'))
+                    if self.cvalue == 21:
+                        print('Computer hit blackjack!')
+                        return 5
+                    if self.cvalue > 21:
+                        print('Computer busted')
+                        return 6
+
+                if stay == False and cstay == False:
+                    return 7
+
 
     #returns an array of percentage chance of drawing a card worth [2,3,4,5,6,7,8,9,10,11]
     def getChances(self):
@@ -59,10 +95,13 @@ class Game:
         if dif >= 11:
             self.chand.append(self.newCard('computer'))
         prob = self.getChances()
-        for i in range(0,8):
-            #chances of going over (drawing a card with a value larger than dif)
-            bad =
-            if prob[i]
+        #chances of going over (drawing a card with a value larger than dif)
+        bad = 0
+        for i in range(int(dif),11):
+            bad += prob(i)
+        if bad > .5:
+            return 'stay'
+        else return 'hit'
 
     #adds a new card to player's hand 'user' for human 'computer' for computer
     def newCard(self, player):
